2120e21593757ec0dc0e212f028358f4471c9845
roifok <skunkyfok@gmail.com>
2019-08-14 16:10:31 +0200

    [Druid] Balance Release

--- Druid/ProfileUI.lua
+++ Druid/ProfileUI.lua
@@ -182,10 +182,237 @@ A.Data.ProfileUI = {
                 },
             },
         },
+        [ACTION_CONST_DRUID_BALANCE] = {             
+            { -- [1] 1st Row                           
+                {
+                    E = "Checkbox", 
+                    DB = "mouseover",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use @mouseover", 
+                        ruRU = "Использовать @mouseover", 
+                        frFR = "Utiliser les fonctions @mouseover",
+                    }, 
+                    TT = { 
+                        enUS = "Will unlock use actions for @mouseover units\nExample: Resuscitate, Healing", 
+                        ruRU = "Разблокирует использование действий для @mouseover юнитов\nНапример: Воскрешение, Хилинг", 
+                        frFR = "Activera les actions via @mouseover\n Exemple: Ressusciter, Soigner",
+                    }, 
+                    M = {},
+                },
+                {
+                    E = "Checkbox", 
+                    DB = "AoE",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use AoE", 
+                        ruRU = "Использовать AoE", 
+                        frFR = "Utiliser l'AoE"
+                    }, 
+                    TT = { 
+                        enUS = "Enable multiunits actions", 
+                        ruRU = "Включает действия для нескольких целей", 
+                        frFR = "Activer les actions multi-unités",
+                    }, 
+                    M = {},
+                }, 
+                {
+                    E = "Checkbox", 
+                    DB = "CDs",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use Cooldowns", 
+                        ruRU = "Использовать Cooldowns", 
+                        frFR = "Utiliser les Cooldowns"
+                    }, 
+                    TT = { 
+                        enUS = "Enable cooldowns usage in rotation", 
+                        ruRU = "Включить использование перезарядки в ротации", 
+                        frFR = "Activer l'usage des cooldowns par la rotation",
+                    }, 
+                    M = {},
+                },                 
+            }, 
+            { -- [2] 2nd Row
+                {
+                    E = "Checkbox", 
+                    DB = "OffGCDasOffGCD",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use spells OffGCD", 
+                        ruRU = "Используйте заклинания OffGCD", 
+                        frFR = "Utiliser les spells OffGCD",
+                    }, 
+                    TT = { 
+                        enUS = "Will force certains spells to be used as off GCD", 
+                        ruRU = "Вынудит определенные заклинания использоваться как вне GCD", 
+                        frFR = "Forcera certains spells à être utilisés sur le GCD",
+                    }, 
+                    M = {},
+                },  
+            },
+            { -- [3] 3rd Row 
+                {
+                    E = "Slider",                                                     
+                    MIN = -1, 
+                    MAX = 100,                            
+                    DB = "BarkskinHP",
+                    DBV = 60, -- Set healthpercentage @60% life. 
+                    ONOFF = true,
+                    L = { 
+                        ANY = A.GetSpellInfo(22812) .. " (%)",
+                    }, 
+                    M = {},
+                },
+                {
+                    E = "Slider",                                                     
+                    MIN = -1, 
+                    MAX = 100,                            
+                    DB = "RenewalHP",
+                    DBV = 40, -- Set healthpercentage @40% life. 
+                    ONOFF = true,
+                    L = { 
+                        ANY = A.GetSpellInfo(108238) .. " (%)",
+                    }, 
+                    M = {},
+                },
+
+            },
+            { -- [4] 4th Row
+			    -- Splash Data
+                {
+                    E = "Checkbox", 
+                    DB = "UseSplashData",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use Splash Data for AoE", 
+                        ruRU = "Использовать данные всплеска для AoE", 
+                        frFR = "Utiliser les données Splash pour AoE",
+                    }, 
+                    TT = { 
+                        enUS = "Only count AoE enemies that are already hit by AoE abilities.", 
+                        ruRU = "Считайте только AoE врагов, которые уже поражены AoE способностями.", 
+                        frFR = "Ne compter que les ennemis qui sont déjà touchés par des capacités de zone.",
+                    }, 
+                    M = {},
+                }, 		
+			    -- Splash Data
+                {
+                    E = "Checkbox", 
+                    DB = "ShowMoonkinFormOOC",
+                    DBV = true,
+                    L = { 
+                        enUS = "Show Moonkin Form Out of Combat", 
+                        ruRU = "Show Moonkin Form Out of Combat", 
+                        frFR = "Show Moonkin Form Out of Combat",
+                    }, 
+                    TT = { 
+                        enUS = "Enable this if you want the addon to show you the Moonkin Form reminder out of combat.", 
+                        ruRU = "Enable this if you want the addon to show you the Moonkin Form reminder out of combat.", 
+                        frFR = "Enable this if you want the addon to show you the Moonkin Form reminder out of combat.",
+                    }, 
+                    M = {},
+                }, 		
+                {
+                    E = "LayoutSpace",                                                                         
+                },
+            }, 
+            { -- [7]
+                {
+                    E = "Header",
+                    L = {
+                        ANY = " -- PvP -- ",
+                    },
+                },
+            },
+            { -- [5] 5th Row     
+                {
+                    E = "Dropdown",                                                         
+                    OT = {
+                        { text = "ON MELEE BURST", value = "ON MELEE BURST" },
+                        { text = "ON COOLDOWN", value = "ON COOLDOWN" },                    
+                        { text = "OFF", value = "OFF" },
+                    },
+                    DB = "FearPvP",
+                    DBV = "ON MELEE BURST",
+                    L = { 
+                        ANY = "PvP " .. A.GetSpellInfo(5782),
+                    }, 
+                    TT = { 
+                        enUS = "@arena1-3, @target, @mouseover, @targettarget\nON MELEE BURST - Only if melee player has damage buffs\nON COOLDOWN - means will use always on melee players\nOFF - Cut out from rotation but still allow work through Queue and MSG systems\nIf you want fully turn it OFF then you should make SetBlocker in 'Actions' tab", 
+                        ruRU = "@arena1-3, @target, @mouseover, @targettarget\nON MELEE BURST - Только если игрок ближнего боя имеет бафы на урон\nON COOLDOWN - значит будет использовано по игрокам ближнего боя по восстановлению способности\nOFF - Выключает из ротации, но при этом позволяет Очередь и MSG системам работать\nЕсли нужно полностью выключить, тогда установите блокировку во вкладке 'Действия'", 
+                    }, 
+                    M = {},
+                },
+                {
+                    E = "Dropdown",                                                         
+                    OT = {
+                        { text = "@arena1", value = 1 },
+                        { text = "@arena2", value = 2 },
+                        { text = "@arena3", value = 3 },
+                        { text = "primary", value = 4 },
+                    },
+                    MULT = true,
+                    DB = "FearPvPUnits",
+                    DBV = {
+                        [1] = true, 
+                        [2] = true,
+                        [3] = true,
+                        [4] = true,
+                    }, 
+                    L = { 
+                        ANY = "PvP " .. A.GetSpellInfo(5782) .. " units",
+                    }, 
+                    TT = { 
+                        enUS = "primary - is @target, @mouseover, @targettarget (these units are depend on toggles above)", 
+                        ruRU = "primary - это @target, @mouseover, @targettarget (эти юниты зависят от чекбоксов наверху)", 
+                    }, 
+                    M = {},
+                },
+            },
+        },
     },
     -- MSG Actions UI
     [7] = {
         [ACTION_CONST_DRUID_FERAL] = { 
+            -- MSG Action Pet Dispell
+            ["dispell"] = { Enabled = true, Key = "PetDispell", LUA = [[
+                return     A.DispellMagic:IsReady(unit, true) and 
+                        (
+                            ( 
+                                not Unit(thisunit):IsEnemy() and 
+                                (
+                                    (
+                                        not InPvP() and 
+                                        Env.Dispel(unit)
+                                    ) or 
+                                    (
+                                        InPvP() and 
+                                        EnemyTeam():PlayersInRange(1, 5)
+                                    ) 
+                                )
+                            ) or 
+                            ( 
+                                Unit(thisunit):IsEnemy() and 
+                                Unit(thisunit):GetRange() <= 5 and 
+                                Action[PlayerSpec].SpellLock:AbsentImun(thisunit, {"TotalImun", "DeffBuffsMagic"}, true) 
+                            )                
+                        ) 
+            ]] },
+            -- MSG Action Pet Kick
+            ["kick"] = { Enabled = true, Key = "Pet Kick", LUA = [[
+                return  SpellInRange(thisunit, Action[PlayerSpec].SpellLock.ID) and 
+                        select(2, CastTime(nil, thisunit)) > 0 and 
+                        Action[PlayerSpec].SpellLock:AbsentImun(thisunit, {"KickImun", "TotalImun", "DeffBuffsMagic"}, true) 
+            ]] },
+            -- MSG Action Fear
+            ["kick"] = { Enabled = true, Key = "Pet Kick", LUA = [[
+                return  SpellInRange(thisunit, Action[PlayerSpec].SpellLock.ID) and 
+                        select(2, CastTime(nil, thisunit)) > 0 and 
+                        Action[PlayerSpec].SpellLock:AbsentImun(thisunit, {"KickImun", "TotalImun", "DeffBuffsMagic"}, true) 
+            ]] },
+        },
+        [ACTION_CONST_DRUID_BALANCE] = { 
             -- MSG Action Pet Dispell
             ["dispell"] = { Enabled = true, Key = "PetDispell", LUA = [[
                 return     A.DispellMagic:IsReady(unit, true) and 
