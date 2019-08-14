f3f91ba57322ff9c43167d1e1b4b6f1dbdb270c0
roifok <skunkyfok@gmail.com>
2019-08-14 14:46:37 +0200

    [Warrior] Protection release

--- Warrior/ProfileUI.lua
+++ Warrior/ProfileUI.lua
@@ -181,10 +181,226 @@ A.Data.ProfileUI = {
                 },
             },
         },
+        [ACTION_CONST_WARRIOR_PROT] = {             
+            { -- [1] 1st Row                           
+                {
+                    E = "Checkbox", 
+                    DB = "mouseover",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use @mouseover", 
+                        ruRU = "Использовать @mouseover", 
+                        frFR = "Utiliser les fonctions @mouseover",
+                    }, 
+                    TT = { 
+                        enUS = "Will unlock use actions for @mouseover units\nExample: Resuscitate, Healing", 
+                        ruRU = "Разблокирует использование действий для @mouseover юнитов\nНапример: Воскрешение, Хилинг", 
+                        frFR = "Activera les actions via @mouseover\n Exemple: Ressusciter, Soigner",
+                    }, 
+                    M = {},
+                },
+                {
+                    E = "Checkbox", 
+                    DB = "AoE",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use AoE", 
+                        ruRU = "Использовать AoE", 
+                        frFR = "Utiliser l'AoE"
+                    }, 
+                    TT = { 
+                        enUS = "Enable multiunits actions", 
+                        ruRU = "Включает действия для нескольких целей", 
+                        frFR = "Activer les actions multi-unités",
+                    }, 
+                    M = {},
+                }, 
+                {
+                    E = "Checkbox", 
+                    DB = "CDs",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use Cooldowns", 
+                        ruRU = "Использовать Cooldowns", 
+                        frFR = "Utiliser les Cooldowns"
+                    }, 
+                    TT = { 
+                        enUS = "Enable cooldowns usage in rotation", 
+                        ruRU = "Включить использование перезарядки в ротации", 
+                        frFR = "Activer l'usage des cooldowns par la rotation",
+                    }, 
+                    M = {},
+                },                 
+            }, 
+            { -- [2] 2nd Row
+                {
+                    E = "Checkbox", 
+                    DB = "OffGCDasOffGCD",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use spells OffGCD", 
+                        ruRU = "Используйте заклинания OffGCD", 
+                        frFR = "Utiliser les spells OffGCD",
+                    }, 
+                    TT = { 
+                        enUS = "Will force certains spells to be used as off GCD", 
+                        ruRU = "Вынудит определенные заклинания использоваться как вне GCD", 
+                        frFR = "Forcera certains spells à être utilisés sur le GCD",
+                    }, 
+                    M = {},
+                },  
+            },
+            { -- [3] 3rd Row 
+                {
+                    E = "Checkbox", 
+                    DB = "UseLastStandToFillShieldBlockDownTime",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use Last Stand to fill Shield Block down time", 
+                        ruRU = "Use Last Stand to fill Shield Block down time", 
+                        frFR = "Use Last Stand to fill Shield Block down time",
+                    }, 
+                    TT = { 
+                        enUS = "Enable this if you want to fill Shield Block down time with Last Stand.", 
+                        ruRU = "Enable this if you want to fill Shield Block down time with Last Stand.", 
+                        frFR = "Enable this if you want to fill Shield Block down time with Last Stand.",
+                    }, 
+                    M = {},
+                }, 
+                {
+                    E = "Checkbox", 
+                    DB = "UseShieldBlockDefensively",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use Shield Block purely defensively", 
+                        ruRU = "Use Shield Block purely defensively", 
+                        frFR = "Use Shield Block purely defensively",
+                    }, 
+                    TT = { 
+                        enUS = "Enable this if you only want to use Shield Block in a defensive manner.", 
+                        ruRU = "Enable this if you only want to use Shield Block in a defensive manner.", 
+                        frFR = "Enable this if you only want to use Shield Block in a defensive manner.",
+                    }, 
+                    M = {},
+                }, 
+                {
+                    E = "Checkbox", 
+                    DB = "UseRageDefensively",
+                    DBV = true,
+                    L = { 
+                        enUS = "Use Rage defensively", 
+                        ruRU = "Use Rage defensively", 
+                        frFR = "Use Rage defensively",
+                    }, 
+                    TT = { 
+                        enUS = "Enable this to prioritize defensive use of Rage.", 
+                        ruRU = "Enable this to prioritize defensive use of Rage.", 
+                        frFR = "Enable this to prioritize defensive use of Rage.",
+                    }, 
+                    M = {},
+                }, 
+            },
+            { -- [4] 4th Row
+                {
+                    E = "LayoutSpace",                                                                         
+                },
+            }, 
+            { -- [7]
+                {
+                    E = "Header",
+                    L = {
+                        ANY = " -- PvP -- ",
+                    },
+                },
+            },
+            { -- [5] 5th Row     
+                {
+                    E = "Dropdown",                                                         
+                    OT = {
+                        { text = "ON MELEE BURST", value = "ON MELEE BURST" },
+                        { text = "ON COOLDOWN", value = "ON COOLDOWN" },                    
+                        { text = "OFF", value = "OFF" },
+                    },
+                    DB = "FearPvP",
+                    DBV = "ON MELEE BURST",
+                    L = { 
+                        ANY = "PvP " .. A.GetSpellInfo(5782),
+                    }, 
+                    TT = { 
+                        enUS = "@arena1-3, @target, @mouseover, @targettarget\nON MELEE BURST - Only if melee player has damage buffs\nON COOLDOWN - means will use always on melee players\nOFF - Cut out from rotation but still allow work through Queue and MSG systems\nIf you want fully turn it OFF then you should make SetBlocker in 'Actions' tab", 
+                        ruRU = "@arena1-3, @target, @mouseover, @targettarget\nON MELEE BURST - Только если игрок ближнего боя имеет бафы на урон\nON COOLDOWN - значит будет использовано по игрокам ближнего боя по восстановлению способности\nOFF - Выключает из ротации, но при этом позволяет Очередь и MSG системам работать\nЕсли нужно полностью выключить, тогда установите блокировку во вкладке 'Действия'", 
+                    }, 
+                    M = {},
+                },
+                {
+                    E = "Dropdown",                                                         
+                    OT = {
+                        { text = "@arena1", value = 1 },
+                        { text = "@arena2", value = 2 },
+                        { text = "@arena3", value = 3 },
+                        { text = "primary", value = 4 },
+                    },
+                    MULT = true,
+                    DB = "FearPvPUnits",
+                    DBV = {
+                        [1] = true, 
+                        [2] = true,
+                        [3] = true,
+                        [4] = true,
+                    }, 
+                    L = { 
+                        ANY = "PvP " .. A.GetSpellInfo(5782) .. " units",
+                    }, 
+                    TT = { 
+                        enUS = "primary - is @target, @mouseover, @targettarget (these units are depend on toggles above)", 
+                        ruRU = "primary - это @target, @mouseover, @targettarget (эти юниты зависят от чекбоксов наверху)", 
+                    }, 
+                    M = {},
+                },
+            },
+        },
     },
     -- MSG Actions UI
     [7] = {
         [ACTION_CONST_WARRIOR_FURY] = { 
+            -- MSG Action Pet Dispell
+            ["dispell"] = { Enabled = true, Key = "PetDispell", LUA = [[
+                return     A.DispellMagic:IsReady(unit, true) and 
+                        (
+                            ( 
+                                not Unit(thisunit):IsEnemy() and 
+                                (
+                                    (
+                                        not InPvP() and 
+                                        Env.Dispel(unit)
+                                    ) or 
+                                    (
+                                        InPvP() and 
+                                        EnemyTeam():PlayersInRange(1, 5)
+                                    ) 
+                                )
+                            ) or 
+                            ( 
+                                Unit(thisunit):IsEnemy() and 
+                                Unit(thisunit):GetRange() <= 5 and 
+                                Action[PlayerSpec].SpellLock:AbsentImun(thisunit, {"TotalImun", "DeffBuffsMagic"}, true) 
+                            )                
+                        ) 
+            ]] },
+            -- MSG Action Pet Kick
+            ["kick"] = { Enabled = true, Key = "Pet Kick", LUA = [[
+                return  SpellInRange(thisunit, Action[PlayerSpec].SpellLock.ID) and 
+                        select(2, CastTime(nil, thisunit)) > 0 and 
+                        Action[PlayerSpec].SpellLock:AbsentImun(thisunit, {"KickImun", "TotalImun", "DeffBuffsMagic"}, true) 
+            ]] },
+            -- MSG Action Fear
+            ["kick"] = { Enabled = true, Key = "Pet Kick", LUA = [[
+                return  SpellInRange(thisunit, Action[PlayerSpec].SpellLock.ID) and 
+                        select(2, CastTime(nil, thisunit)) > 0 and 
+                        Action[PlayerSpec].SpellLock:AbsentImun(thisunit, {"KickImun", "TotalImun", "DeffBuffsMagic"}, true) 
+            ]] },
+        },
+        [ACTION_CONST_WARRIOR_PROT] = { 
             -- MSG Action Pet Dispell
             ["dispell"] = { Enabled = true, Key = "PetDispell", LUA = [[
                 return     A.DispellMagic:IsReady(unit, true) and 
