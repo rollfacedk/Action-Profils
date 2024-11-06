a05f6f073eb6d14dee77905abd0615f3f08077d7
rollfacedk <rollfacedk@gmail.com>
2024-11-06 11:00:36 -0500

    Update AAARestoShaman.lua

--- AAARestoShaman/AAARestoShaman.lua
+++ AAARestoShaman/AAARestoShaman.lua
@@ -401,13 +401,13 @@ local function MyRoutine()
 			end
 		end
 
-		if Player:AffectingCombat() then
-			if (Player:BuffDown(S.HealingRainBuff)) and MouseOver:Exists()  then
-				if Cast(S.SurgingTotem) then return end
-			elseif S.TotemicProjection:IsCastable() and MouseOver:Exists() then
-				if Cast(S.TotemicProjection) then return end
-			end
-		end
+		-- if Player:AffectingCombat() then
+			-- if (Player:BuffDown(S.HealingRainBuff)) and MouseOver:Exists()  then
+				-- if Cast(S.SurgingTotem) then return end
+			-- elseif S.TotemicProjection:IsCastable() and MouseOver:Exists() then
+				-- if Cast(S.TotemicProjection) then return end
+			-- end
+		-- end
 
 		if (HealingEngine:MembersUnderPercentage(85, nil, 30) >= 3 and Player:BuffUp(S.HighTideBuff) or HealingEngine:MembersUnderPercentage(50, nil, 30) >= 3) or 
 			(Player:BuffUp(S.HighTideBuff) and (HealingEngine:DebuffTotal(S.EnvelopingShadowflame, 30) >= 3 or HealingEngine:DebuffTotal(S.VoidRift, 30) >= 3 or 
