356f070f1f98012722f1c65c591912c2f501f5e9
rollfacedk <rollfacedk@gmail.com>
2024-11-19 00:15:05 -0500

    Add files via upload

--- AAACastweaverMONK/AAACastweaverMONK.lua
+++ AAACastweaverMONK/AAACastweaverMONK.lua
@@ -1,5 +1,5 @@
 local function MyRoutine()
-	local Author = 'Castweaver'
+	local Author = 'CASTER'
 	local SpecID = 270 --Unholy  --https://wowpedia.fandom.com/wiki/API_GetSpecializationInfo
 
 	--HR HEADER
@@ -36,13 +36,13 @@ local function MyRoutine()
 	local AoEON = MainAddon.AoEON
 	local CDsON = MainAddon.CDsON
 	local CastCycleAlly = MainAddon.CastCycleAlly;
+	local ForceCastAlly = MainAddon.ForceCastAlly;
 	local CastTargetIfAlly = MainAddon.CastTargetIfAlly;
 	local CastCycle = MainAddon.CastCycle;
 	local CastAlly = MainAddon.CastAlly;
 	local mathmax    = math.max
 	local mathmin    = math.min
 	local TANKS, HEALERS, MEMBERS, DPS, PRIORITY;
-
 
 	--ALL SPELLS ARE LOCATED ON Rotations/CLASS_SPELL.lua, if one is missing tells us about it.
 	local S = {
@@ -136,6 +136,7 @@ local function MyRoutine()
 		SpinningCraneKick = Spell(101546),
 		ChiBurst = Spell(461404),
 		LifeCocoon = Spell(116849),
+		Yulonbaby = Spell(389422),
 		-- Talents
 		InvokeYulonTheJadeSerpent = Spell(322118),
 		InvokeChiJiTheRedCrane = Spell(325197),
@@ -215,41 +216,260 @@ local function MyRoutine()
 	end
 
 
+	local function Trinkets()
+	
+		if true then
+		  local ItemToUse, ItemSlot, ItemRange = Player:GetUseableItems(OnUseExcludes)
+		  if ItemToUse and Target:IsInRange(ItemRange) then
+			local DisplayStyle = true
+			if ItemSlot ~= 13 and ItemSlot ~= 14 then DisplayStyle = true end
+			if ((ItemSlot == 13 or ItemSlot == 14) and true) or (ItemSlot ~= 13 and ItemSlot ~= 14 and true) then
+			  if Cast(ItemToUse) then return "Generic use_items for " .. ItemToUse:Name(); end
+			end
+		  end
+		end
+	end
+
+	local MW = {}
+	MW.Yulon = {}
+	MW.Yulon.Active = false
+	MW.Yulon.GUID = 0
+	MW.Yulon.SummonTime = 0
+	HL:RegisterForSelfCombatEvent(
+	function(...)
+		local destGUID, _, _, _, spellID = select(8, ...)
+		if spellID == 322118 then
+		MW.Yulon.Active = true
+		MW.Yulon.GUID = destGUID
+		MW.Yulon.SummonTime = GetTime()
+		end
+	end
+	, "SPELL_SUMMON"
+	)
+
+	HL:RegisterForCombatEvent(
+	function(...)
+		local destGUID = select(8, ...)
+		if destGUID == MW.Yulon.GUID then
+		MW.Yulon.Active = false
+		MW.Yulon.GUID = 0
+		MW.Yulon.SummonTime = 0
+		end
+	end
+	, "UNIT_DIED"
+	)
+
+	local function  Test(UnitTarget)
+		return Player:HealthPercentage() <= 100 and UnitTarget:BuffDown(S.EnvelopingMist)
+	end;
+
+	local function RenewingMistFunc(UnitTarget)
+		return UnitTarget:BuffDown(S.RenewingMistBuff)
+	end;
+
+	local function VivifyFunc(UnitTarget)
+		return UnitTarget:HealthPercentage() <= 65 or UnitTarget:DebuffUp(S.EnvelopingShadowflame) or UnitTarget:DebuffUp(S.VoidRift) or UnitTarget:DebuffUp(S.CurseOfEntropy) or UnitTarget:DebuffUp(S.CorruptedCoating)
+	end;
+
+	local function SoothingMistFunc(UnitTarget)
+		return UnitTarget:HealthPercentageFlat() <= 90 or UnitTarget:DebuffUp(S.EnvelopingShadowflame) or UnitTarget:DebuffUp(S.VoidRift) or UnitTarget:DebuffUp(S.CurseOfEntropy) or UnitTarget:DebuffUp(S.CorruptedCoating)
+
+	end;
+
+	local function LifeCocoonFunc(UnitTarget)
+		return UnitTarget:HealthPercentageFlat() <= 25
+	end;
+
+	-----
+
+	local function VivifyEmergencyFunc(UnitTarget)
+		return UnitTarget:HealthPercentageFlat() <= 25
+	end;
+
+	local function EnvelopingMistSoothingFunc(UnitTarget)
+		return UnitTarget:BuffDown(S.EnvelopingMist) and (UnitTarget:HealthPercentage() <= 85 or UnitTarget:DebuffUp(S.EnvelopingShadowflame) or UnitTarget:DebuffUp(S.VoidRift) or UnitTarget:DebuffUp(S.CurseOfEntropy) or UnitTarget:DebuffUp(S.CorruptedCoating))
+	end;
+
+	local function VivifySoothingFunc(UnitTarget)
+		return  (UnitTarget:HealthPercentage() <= 85 or UnitTarget:DebuffUp(S.EnvelopingShadowflame) or UnitTarget:DebuffUp(S.VoidRift) or UnitTarget:DebuffUp(S.CurseOfEntropy) or UnitTarget:DebuffUp(S.CorruptedCoating))
+	end;
 
 
+
+	--- ===== Rotation Variables =====
+	local Enemies5y
+	local EnemiesCount5
+
+	S.ThunderFocusTea.offGCD = true
+	S.LifeCocoon.offGCD = true
+	S.EnvelopingMist.offGCD = true
 	
 	local function MainRotation()
 
 		TANKS, HEALERS, MEMBERS, DPS, PRIORITY = HealingEngine:Fetch()
 
-		if S.EnvelopingMist:IsCastable() and Player:IsChanneling(S.SoothingMist) then
-			if Cast(S.EnvelopingMist, Player) then return "EnvelopingMist" end
+
+		-- Unit Update
+		Enemies5y = Player:GetEnemiesInMeleeRange(5) -- Multiple Abilities
+		if AoEON() then
+		EnemiesCount5 = #Enemies5y
+		else
+		EnemiesCount5 = 1
 		end
 
-		if S.Vivify:IsCastable() and Player:IsChanneling(S.SoothingMist) then
-			if Cast(S.Vivify, Player) then return "EnvelopingMist" end
+		if true then
+			-- Calculate fight_remains
+			BossFightRemains = HL.BossFightRemains()
+			FightRemains = BossFightRemains
+			if FightRemains == 11111 then
+			  FightRemains = HL.FightRemains(Enemies5y, false)
+			end
+		
+			-- Are we tanking?
+			
+			-- Update CombatTime, which is used in many spell suggestions
+			CombatTime = HL.CombatTime()
+		end
+
+		if Player:IsChanneling(S.ManaTea) then
+			if not Player:IsInRaidArea() then
+				if HealingEngine.LowestHP(true, 40) <= 50 or Player:ManaPercentage() >= 95 then
+					MainAddon.SetTopColor(1, "Stop Casting")
+				end
+			end;
+		end
+
+		if Player:IsChanneling(S.ManaTea) or Player:IsChanneling(S.CelestialConduit) or Player:IsChanneling(S.SpinningCraneKick) then
+			return
+		end
+
+		-- if Player:IsCasting(S.Vivify) or Player:IsCasting(S.EnvelopingMist) then
+		-- 	MainAddon.SetTopColor(1, "Stop Casting")
+		-- end
+
+		-- if Player:IsChanneling(S.SoothingMist)then
+		-- 	if (Focus:HealthPercentage() > 99) then
+		-- 		if MainAddon.SetTopColor(1, "Stop Casting") then end
+		-- 	end
+		-- end
+
+		if S.LifeCocoon:CooldownUp() and Player:AffectingCombat() then
+			if CastCycleAlly(S.LifeCocoon, MEMBERS, LifeCocoonFunc) then return "" end
+		end
+
+		if S.ExpelHarm:CooldownUp() and Player:HealthPercentageFlat() <= 65 then
+			if  Cast(S.ExpelHarm, Player) then return end
+		end
+
+		if S.FortifyingBrew:CooldownUp() and Player:HealthPercentageFlat() <= 35 then
+			if Cast(S.FortifyingBrew) then return end
+		end
+
+		if S.Revival:CooldownUp() and not MW.Yulon.Active and (HealingEngine:MembersUnderPercentage(45, nil, 40) >= 4 or HealingEngine:MembersUnderPercentage(35, nil, 40) >= 3) then
+			if  Cast(S.Revival) then return end
+		end
+
+		if S.ManaTea:CooldownUp() and (HealingEngine:LowestHP() > 75 or not Player:IsInParty()) and S.ManaTea:Count() >= 10 and Player:ManaPercentage() <= 75 then
+			if  Cast(S.ManaTea) then return end
+		end
+
+		if S.RenewingMist:CooldownUp() and (HealingEngine:LowestHP() > 65 or Player:IsMoving()) then
+			if S.ThunderFocusTea:CooldownUp() and Player:AffectingCombat() then
+				if Cast(S.ThunderFocusTea) then return end
+			end
+			if CastCycleAlly(S.RenewingMist, MEMBERS, RenewingMistFunc) then return end
+		end
+
+		if Player:BuffDown(S.Yulonbaby) and (HealingEngine:MembersUnderPercentage(85, nil, 40) >= 3 or HealingEngine:DebuffTotal(S.EnvelopingShadowflame, 30) >= 3 or HealingEngine:DebuffTotal(S.VoidRift, 30) >= 3 or HealingEngine:DebuffTotal(S.CurseOfEntropy, 30) >= 3 or HealingEngine:DebuffTotal(S.CorruptedCoating, 30) >= 3) then
+	
+			if S.SheilunsGift:CooldownUp() and not Player:IsMoving() and S.SheilunsGift:Count() >= 5 and HealingEngine:LowestHP() >= 30 then
+				if  Cast(S.SheilunsGift, Player) then return end
+			end
+			
+			if S.InvokeYulonTheJadeSerpent:CooldownUp() then
+				if  Cast(S.InvokeYulonTheJadeSerpent) then return end
+			end
+
+			if S.CelestialConduit:CooldownUp() then
+				if Cast(S.CelestialConduit) then return end
+			end
+		end
+
+		if TargetIsValid() and Target:IsInMeleeRange(5) and HealingEngine:LowestHP() > 85  then
+
+			if Player:IsChanneling(S.SoothingMist)then			
+				if MainAddon.SetTopColor(1, "Stop Casting") then end			
+			end
+	
+			if S.TouchofDeath:IsReady() then
+				if Cast(S.TouchofDeath) then return end
+			end
+	
+			if S.ChiBurst:CooldownUp() then
+				if  Cast(S.ChiBurst) then return end
+			end
+	
+			if EnemiesCount5 >= 4 or Player:BuffUp(S.DanceofChijiBuff) then
+				if S.SpinningCraneKick:IsReady() then
+					if  Cast(S.SpinningCraneKick) then return end
+				end
+			end
+	
+			if S.RisingSunKick:IsReady() then
+				if  Cast(S.RisingSunKick) then return end
+			end
+	
+			if S.BlackoutKick:IsReady() and Player:BuffStack(S.TeachingsoftheMonasteryBuff) >= 4 then
+				if  Cast(S.BlackoutKick) then return end
+			end
+	
+			if S.TigerPalm:IsReady() then
+				if  Cast(S.TigerPalm) then return end
+			end
 		end
 
 		if S.SoothingMist:IsCastable() and not Player:IsChanneling(S.SoothingMist) then
-			if Cast(S.SoothingMist, Player) then return "SoothingMist" end
+			if CastCycleAlly(S.SoothingMist, MEMBERS, SoothingMistFunc) then return end
+		end
+
+		if S.Vivify:CooldownUp() and Player:IsMoving() and Player:BuffUp(S.VivaciousVivificationBuff) then
+			if CastCycleAlly(S.Vivify, MEMBERS, VivifyFunc) then return "vivify" end
+		end
+
+		if S.Vivify:CooldownUp() and Player:IsChanneling(S.SoothingMist) then
+			if CastCycleAlly(S.Vivify, MEMBERS, VivifyEmergencyFunc) then return "vivify" end
 		end
 		
+		if S.EnvelopingMist:CooldownUp() and Player:IsChanneling(S.SoothingMist) and not Player:PrevGCD(1,S.EnvelopingMist) and not Player:PrevGCD(2,S.EnvelopingMist) and not Player:PrevGCD(3,S.EnvelopingMist) then
+			if CastCycleAlly(S.EnvelopingMist, TANKS, EnvelopingMistSoothingFunc) then return "vivify" end
+		end
+		
+		if S.Vivify:CooldownUp() and Player:IsChanneling(S.SoothingMist) then
+			if CastCycleAlly(S.Vivify, MEMBERS, VivifySoothingFunc) then return "vivify" end
+		end
+	
 	end
+
 
 	
 	MainAddon.SetCustomAPL(Author, SpecID, MainRotation, Init)
 
-	    local WWOldIsCastable
+	local WWOldIsCastable
     WWOldIsCastable = HL.AddCoreOverride("Spell.IsCastable",
-            function(self, ignoreSettings, ignoreMovement, bypassRecovery, ignoreChannel)
-            
-                if self:ID() == S.Vivify:ID() then
-                    ignoreChannel = true
-                end
+            function(self, ignoreSettings, ignoreMovement, bypassRecovery, ignoreChannel)		
+				if self == S.EnvelopingMist then
+					if Player:IsMoving() and (Player:BuffUp(S.ThunderFocusTea) or Player:BuffStack(S.InvokeChiJiBuff) == 3) then
+						ignoreMovement = true
+					end
+				end
 
-				if self:ID() == S.EnvelopingMist:ID() then
-                    ignoreChannel = true
-                end
+				-- if self == S.Vivify and Player:IsChanneling(S.SoothingMist)  then
+				-- 	ignoreChannel = true
+				-- end
+
+				-- if self == S.EnvelopingMist and Player:IsChanneling(S.SoothingMist)  then
+				-- 	ignoreChannel = true
+				-- end
 
                 local BaseCheck, Reason = WWOldIsCastable(self, ignoreSettings, ignoreMovement, bypassRecovery, ignoreChannel)
                 return BaseCheck, Reason
